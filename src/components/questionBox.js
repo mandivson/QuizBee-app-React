4852ac798455e720e6ee518e445e567792bc5cf1
mandivson <manishranjan2606@gmail.com>
2021-05-23 23:02:23 +0530

    Added all files

---
+++ src/components/questionBox.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+const QuestionBox = ({question, options, selected}) => {
+    const [answer, setAnswer] = useState(options);
+    return (
+        <div className="questionBox">
+            <div className="question">{question}</div>
+            
+            {answer.map((text, index) => (
+                <button key={index} className="answerBtn" onClick={() => {
+                    setAnswer([text]);
+                    selected(text);
+                }}>
+                    {text}
+                </button>
+            ))}
+
+        </div>
+    )
+}
+
+export default QuestionBox;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,70 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "./assets/style.css";
+import quizService from "./quizService";
+import QuestionBox from "./components/questionBox";
+import Result from "./components/Result";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class QuizBee extends Component {
+    state = {
+        questionBank : [],
+        score: 0,
+        responses: 0
+    }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    getQuestions = () => {
+        quizService().then(question => {
+            this.setState({
+                questionBank: question
+            });
+        });
+    }
+
+    computeAnswer = (answer, correctAnswer) => {
+        if(answer === correctAnswer) {
+            this.setState({
+                score: this.state.score + 1
+            });
+        }
+        this.setState({
+            responses: this.state.responses < 5 ? this.state.responses + 1 : 5
+        });
+    }
+
+    playAgain = () => {
+        this.getQuestions();
+        this.setState({
+            score: 0,
+            responses: 0
+        });
+    }
+
+    componentDidMount() {
+        this.getQuestions();
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div className="title">QuizBee</div>
+
+                {this.state.questionBank.length > 0 && this.state.responses < 5 &&
+                this.state.questionBank.map(
+                    ({question, answers, correct, questionId}) => (
+                        <QuestionBox 
+                            question={question} 
+                            options={answers}
+                            key={questionId}
+                            selected={answer => this.computeAnswer(answer, correct)}
+                        />
+                    )
+                )}
+
+
+                {this.state.responses === 5 ? (<Result score={this.state.score} playAgain={this.playAgain} />) : null}
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<QuizBee />, document.getElementById("root"));
